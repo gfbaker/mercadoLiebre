9d639a31a5102c7dd4c0b89efd77e110563dde13
gfbaker <g.baker.9@gmail.com>
2021-10-11 12:50:25 -0300

    Subiendo proyecto de MercadoLiebre

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded());
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get('/ayuda', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/ayuda.html'));
+});
+
+app.get('/ofertas', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/ofertas.html'));
+});
+
+app.get('/vender', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/vender.html'));
+});
+
+/*Formulario de Registro*/
+
+app.get('/crearCuenta', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/crearCuenta.html'));
+});
+
+app.post('/crearCuenta', (req, res) => {
+    res.redirect('/');
+});
+
+/**/
+
+/*Formulario de usuario registrado*/
+app.get('/ingresa', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/ingresa.html'));
+});
+
+app.post('/ingresa', (req, res) => {
+    res.redirect('/');
+});
+
+/**/
+
+app.get('/tiendasOficiales', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/tiendasOficiales.html'));
+});
+
+
+app.get('/miscompras', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/miscompras.html'));
+});
---
+++ estilos.md
@@ -0,0 +1,23 @@
+Paleta de colores:
+Amarillo: #EAC926;
+Azul: #03264C;
+Celeste: #1259c3;
+Gris oscuro: #666;
+Gris claro: #ebebeb;
+Verde: #39b54a;
+
+======================
+
+Tamaños Tipográficos
+Cuerpos de texto: 12px
+Titulares (Basado en tu última visita/Ofertas): 24px
+Precios: 18px
+Descuentos: 16px
+Descripciones: 16px
+
+=======================
+
+Iconos:
+
+El departamento de diseño usó los íconos de este sitio:
+https://fontawesome.com/icons?d=gallery&m=free
