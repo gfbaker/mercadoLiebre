1813441ed0e6f7e9f74290f970cde90c188d84c5
gfbaker <g.baker.9@gmail.com>
2021-10-12 01:11:28 -0300

    pruebo lo que me dice nico en slack

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ app.use(express.static('public'));
 
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo en el puerto 3000");
+    
 });
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log("Servidor corriendo en el puerto 3000");
+}
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'));
